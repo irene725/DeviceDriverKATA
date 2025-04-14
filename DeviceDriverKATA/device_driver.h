b96ed52e4a2dcac199dd28450c283fc2020e7972
heejeong <irene725k@gmail.com>
2025-04-15 08:54:30 +0900

    first commit

---
+++ DeviceDriverKATA/device_driver.h
@@ -0,0 +1,12 @@
+#pragma once
+#include "flash_memory_device.h"
+
+class DeviceDriver {
+ public:
+  DeviceDriver(FlashMemoryDevice *hardware);
+  int read(long address);
+  void write(long address, int data);
+
+ protected:
+  FlashMemoryDevice *m_hardware;
+};
